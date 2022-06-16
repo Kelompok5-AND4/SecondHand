4bd36084ec8032a98a643b46f1456ab8c0b2123d
koirul92 <koirulanwar98@gmail.com>
2022-06-16 16:54:49 +0700

    add feature search logic and add search fragment

--- app/src/main/java/com/igdev/secondhand/MainActivity.kt
+++ app/src/main/java/com/igdev/secondhand/MainActivity.kt
@@ -2,11 +2,20 @@ package com.igdev.secondhand
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.igdev.secondhand.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.menuNavigation.setupWithNavController(navController)
+
     }
 
 }
