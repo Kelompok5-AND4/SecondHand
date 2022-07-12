087924af78786bdf2d63c297a6a20f274d19afd2
rizqifakhri1 <rizqifakhri@gmail.com>
2022-07-12 19:44:27 +0700

    Detail Penawaran Masuk

--- app/src/main/java/com/igdev/secondhand/ui/MainActivity.kt
+++ app/src/main/java/com/igdev/secondhand/ui/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+//test
         setFullScreen(window)
         lightStatusBar(window)
 
